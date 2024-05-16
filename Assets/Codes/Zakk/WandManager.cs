4461007e0a7bf52377d1ac07dd54aab44f86d8f7
Zakk <zakk.norman@elev.ga.lbs.se>
2024-05-16 14:56:15 +0200

    WandManager och elementSO

---
+++ Assets/Codes/Zakk/WandManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WandManager : MonoBehaviour
+{
+
+    [SerializeField] private GameObject currentSpellform;
+
+    [SerializeField] private ElementSO element;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CastSpell();
+        }
+    }
+
+
+    public void SetSpellInfo(GameObject form, ElementSO elementSO)
+    {
+        currentSpellform = form;
+        element = elementSO;
+    }
+
+    public void CastSpell()
+    {
+        GameObject spellObj = Instantiate(currentSpellform, transform.position + transform.forward * 4, Quaternion.identity);
+        Spell spellScript = spellObj.GetComponent<Spell>();
+        spellScript.SetColor(element.elementColor);
+        spellScript.SetLayer(element.elementAttackLayer);
+    }
+
+}
