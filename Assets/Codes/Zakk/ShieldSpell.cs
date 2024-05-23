29b616bcfa0d157038572a5f6eb8ca9712f23bbf
Zakk <zakk.norman@elev.ga.lbs.se>
2024-05-23 15:20:30 +0200

    ShieldSpell Script

--- Assets/Codes/Zakk/ShieldSpell.cs
+++ Assets/Codes/Zakk/ShieldSpell.cs
@@ -2,17 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShieldSpell : MonoBehaviour
+public class ShieldSpell : Spell
 {
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        ShieldManager.instance.ResetShields(gameObject);
     }
 }
