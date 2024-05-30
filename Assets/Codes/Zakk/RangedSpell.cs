fe383aca4527fceaabf5554ced60063e48791c03
Zakk <zakk.norman@elev.ga.lbs.se>
2024-05-30 14:17:47 +0200

    melee spell + lifetime for range + shieldspell lifetime

--- Assets/Codes/Zakk/RangedSpell.cs
+++ Assets/Codes/Zakk/RangedSpell.cs
@@ -9,11 +9,26 @@ public class RangedSpell : Spell
     [SerializeField] private float speed;
     [SerializeField] private float intensity;
 
+    float lifetime = 20;
+    float timer = 0;
+
     [SerializeField] private VisualEffect visualEffect;
+
+
 
     private void Start()
     {
         visualEffect.SetVector4("FrontColor", new Vector4(color.r * intensity, color.g*intensity, color.b*intensity, color.a*intensity));
+    }
+
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > lifetime)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void FixedUpdate()
