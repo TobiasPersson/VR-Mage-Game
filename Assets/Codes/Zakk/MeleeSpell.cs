ecab840475e6fb896d835fb9f07d13be79f2aef5
Zakk <zakk.norman@elev.ga.lbs.se>
2024-05-30 15:09:03 +0200

    Melee Visual

--- Assets/Codes/Zakk/MeleeSpell.cs
+++ Assets/Codes/Zakk/MeleeSpell.cs
@@ -6,12 +6,21 @@ public class MeleeSpell : Spell
 {
 
     public GameObject rightHandController;
+    [SerializeField] private MeshRenderer meshrend;
+    Material mat;
+
+    private void Awake()
+    {
+        mat = meshrend.material;
+    }
 
     private void Start()
     {
         rightHandController = FindObjectOfType<WandManager>().rightHandController;
         transform.SetParent(rightHandController.transform);
         transform.localPosition = new Vector3(0, 2, 0);
+        mat.SetColor("_LitColor", color);
+        mat.SetColor("_DarkColor", color);
     }
 
 
